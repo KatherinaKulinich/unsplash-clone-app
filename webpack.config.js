e21aebf28c93450439a2429c35cb33b2a2c07498
KatherinaKulinich <kulinichcatherina@gmail.com>
2022-12-22 19:55:34 +0200

    html done

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');     
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CssMimimizerPlugin = require('css-minimizer-webpack-plugin');
+const { config } = require('process');
+
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new CssMimimizerPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './js/index.js',
+        button: './js/button.js'                    
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: filename('js')
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@js': path.resolve(__dirname, 'src/js'),
+            '@': path.resolve(__dirname, 'src'),
+        }
+    },
+    optimization: optimization(),
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpe?g|svg|)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[name][ext]'
+                }
+            },
+            {
+                test: /\.(ttf|woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'font/[name][ext]'
+                }
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            }
+        ],
+    },
+};  
